990c3316d60c7b9e0e1d439f6935a7853530e589
Ju <50602425+jumalley@users.noreply.github.com>
2023-10-31 20:13:37 +0100

    Add files via upload

---
+++ skinchanger/config.lua
@@ -0,0 +1,608 @@
+Config = {}
+
+
+Config.Components = {{
+    name = 'sex',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'mom',
+    value = 21,
+    min = 21,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'dad',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'face_md_weight',
+    value = 50,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'skin_md_weight',
+    value = 50,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_1',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_2',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_3',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_4',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_5',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'nose_6',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'cheeks_1',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'cheeks_2',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'cheeks_3',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'lip_thickness',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'jaw_1',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'jaw_2',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'chin_1',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'chin_2',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'chin_3',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'chin_4',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'neck_thickness',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'hair_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'hair_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'hair_color_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'hair_color_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65
+}, {
+    name = 'tshirt_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 8
+}, {
+    name = 'tshirt_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'tshirt_1'
+}, {
+    name = 'torso_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 11
+}, {
+    name = 'torso_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'torso_1'
+}, {
+    name = 'decals_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 10
+}, {
+    name = 'decals_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'decals_1'
+}, {
+    name = 'arms',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'arms_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'pants_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.8,
+    camOffset = -0.5,
+    componentId = 4
+}, {
+    name = 'pants_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.8,
+    camOffset = -0.5,
+    textureof = 'pants_1'
+}, {
+    name = 'shoes_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.8,
+    camOffset = -0.8,
+    componentId = 6
+}, {
+    name = 'shoes_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.8,
+    camOffset = -0.8,
+    textureof = 'shoes_1'
+}, {
+    name = 'mask_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    componentId = 1
+}, {
+    name = 'mask_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    textureof = 'mask_1'
+}, {
+    name = 'bproof_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 9
+}, {
+    name = 'bproof_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'bproof_1'
+}, {
+    name = 'chain_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    componentId = 7
+}, {
+    name = 'chain_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    textureof = 'chain_1'
+}, {
+    name = 'helmet_1',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    componentId = 0
+}, {
+    name = 'helmet_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    textureof = 'helmet_1'
+}, {
+    name = 'glasses_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    componentId = 1
+}, {
+    name = 'glasses_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.6,
+    camOffset = 0.65,
+    textureof = 'glasses_1'
+}, {
+    name = 'watches_1',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 6
+}, {
+    name = 'watches_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'watches_1'
+}, {
+    name = 'bracelets_1',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 7
+}, {
+    name = 'bracelets_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'bracelets_1'
+}, {
+    name = 'bags_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    componentId = 5
+}, {
+    name = 'bags_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15,
+    textureof = 'bags_1'
+}, {
+    name = 'eye_color',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eye_squint',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_4',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_5',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'eyebrows_6',
+    value = 0,
+    min = -10,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'makeup_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'makeup_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'makeup_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'makeup_4',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'lipstick_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'lipstick_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'lipstick_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'lipstick_4',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'ears_1',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.4,
+    camOffset = 0.65,
+    componentId = 2
+}, {
+    name = 'ears_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65,
+    textureof = 'ears_1'
+}, {
+    name = 'chest_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'chest_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'chest_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'bodyb_1',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'bodyb_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.75,
+    camOffset = 0.15
+}, {
+    name = 'bodyb_3',
+    value = -1,
+    min = -1,
+    zoomOffset = 0.4,
+    camOffset = 0.15
+}, {
+    name = 'bodyb_4',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.15
+}, {
+    name = 'age_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'age_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'blemishes_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'blemishes_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'blush_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'blush_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'blush_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'complexion_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'complexion_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'sun_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'sun_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'moles_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'moles_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'beard_1',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'beard_2',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'beard_3',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}, {
+    name = 'beard_4',
+    value = 0,
+    min = 0,
+    zoomOffset = 0.4,
+    camOffset = 0.65
+}}
---
+++ skinchanger/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes'
+
+description 'Official ESX-Legacy resource for handling the Player`s Skin'
+
+version '1.9.4'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
